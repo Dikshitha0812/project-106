81154c17b021bd5e70ccfbdd03d6e22a871f6486
Dikshitha0812 <71216869+Dikshitha0812@users.noreply.github.com>
2021-06-08 09:46:47 +0530

    Update and rename main.py to marks.py

--- marks.py
+++ marks.py
@@ -1,5 +1,5 @@
-import plotly.express as px
 import csv
+import plotly.express as px
 import numpy as np
 
 def plotFigure(data_path):
@@ -9,23 +9,22 @@ def plotFigure(data_path):
         fig.show()
 
 def getDataSource(data_path):
-    marks_in_percentage = []
-    days_present = []
+    marks = []
+    attendance = []
     with open(data_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            marks_in_percentage.append(float(row["Marks In Percentage"]))
-            days_present.append(float(row["Days Present"]))
+            marks.append(float(row["Marks In Percentage"]))
+            attendance.append(float(row["Days Present"]))
 
-    return {"x" : marks_in_percentage, "y": days_present}
+    return {"x" : marks, "y": attendance}
 
 def findCorrelation(datasource):
     correlation = np.corrcoef(datasource["x"], datasource["y"])
-    print("Correlation between Marks in percentage and Days present :-  \n--->",correlation[0,1])
+    print("Correlation between marks and attendance :-  \n--->",correlation)
 
 def setup():
-    data_path  = "./data/Student Marks vs Days Present.csv"
-
+    data_path  = "./marks.csv"
     datasource = getDataSource(data_path)
     findCorrelation(datasource)
     plotFigure(data_path)
